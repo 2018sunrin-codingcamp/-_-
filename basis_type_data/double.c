6cfaa7a792f4fa22f6c565087da6cf93f4fe350a
iwin2471 <iwin12372@gmail.com>
2018-01-22 14:30:44 +0900

    double data type

---
+++ basis_type_data/double.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+  double a = 12.26; // 실수형 double 선언및 a에 12.26대입
+  float b = 10.27; // 실수형 float 선언및 a에 10.27대입
+  printf("%lf\n", a); //%lf는 double 뜻함 a를 출력
+  printf("%f\n", b); //%f는 실수형 뜻함 a를 출력
+  return 0; //함수를 종료하겠다;
+}
