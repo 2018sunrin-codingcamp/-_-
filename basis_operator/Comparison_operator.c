0a8f64bf7a0a7cb09c2a77a4de77244d789530c6
iwin2471 <iwin247@edcan.kr>
2018-01-24 20:07:45 +0900

    add comparison operator

---
+++ basis_operator/Comparison_operator.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+	int a=10, b=20;
+	printf(“%d\n”, a==b);
+	printf(“%d\n”, a<b);
+	printf(“%d\n”, a!=b);
+  return 0;
+}
+
+/*
+  A==B A와B가 같은가?
+  A<B A가 B보다 작은가
+  A>B A가 B보다 큰가
+  A<=B A가 B보다 작거나 같냐
+  A>=B A가 B보다 크거나 같냐
+  A!=B A랑 B랑 다르냐
+*/
