9e15291abfbafeca9f92504a7044293109d85fa3
Younjune Kim <iwin12372@gmail.com>
2018-01-27 20:08:23 +0900

    Revert "add operator"

--- basis_operator/remainder.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main() {
-	int a=4, b=2; //정수 a에 4대입 정수 b에 2대입
-	int result = a % b; // a나누기 b의 나머지 값을 result에 입력
-  printf(“ %d나누기 %d의 나머지 값은 %d”, a,b,result);
-  return 0;
-}
